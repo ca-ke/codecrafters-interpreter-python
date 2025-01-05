b2253f7cefe6ad666a8e9ed5764b8553175a8d71
ca-ke <minhare.caique@gmail.com>
2025-01-05 13:42:14 -0300

    refactor(main): Ajusta espaçamento

--- app/main.py
+++ app/main.py
@@ -16,10 +16,12 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-     if file_contents:
-         raise NotImplementedError("Scanner not implemented")
-     else:
-         print("EOF  null") # Placeholder, remove this line when implementing the scanner
+        if file_contents:
+            raise NotImplementedError("Scanner not implemented")
+        else:
+            print(
+                "EOF  null"
+            )  # Placeholder, remove this line when implementing the scanner
 
 
 if __name__ == "__main__":
