1e43eeb5410e205fa40ec824d85c2b3a2a911279
ca-ke <minhare.caique@gmail.com>
2025-01-05 22:21:40 -0300

    CLI tests (cli-test-1736126500228)

--- app/scanner.py
+++ app/scanner.py
@@ -114,7 +114,14 @@ class Scanner:
     def extract_identifier(self) -> None:
         while self.is_alpha_numeric(self.peek()):
             self.advance()
-        self.add_token(TokenType.IDENTIFIER)
+
+        lexeme = self.source[self.start : self.current]
+        token_type = next(
+            (t for t in TokenType.values() if t.name == lexeme),
+            TokenType.IDENTIFIER,
+        )
+        print(token_type)
+        self.add_token(token_type)
 
     def is_alpha_numeric(self, c: str) -> bool:
         return c.isnumeric() or self.is_alpha(c)
